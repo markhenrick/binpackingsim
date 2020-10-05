9374cbc9431dce91ebe5220fcd2adbdb1661f831
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2020-10-05 20:55:24 +0100

    Add graphical output

--- drive.py
+++ drive.py
@@ -1,29 +1,33 @@
 class Drive:
     def __init__(self, size):
+        assert size > 0
         self._size = size
-        self._graph = [ size ]
+        self._graph = [ 0 ]
     
     def __str__(self):
-        return f"Drive(size={self.size()}, graph={self.graph()})"
+        return f"Drive(size={self.size()}, graph={self.graph_absolute()})"
     
-    def graph(self):
+    def graph_absolute(self):
         return self._graph
+    
+    def graph_relative(self):
+        return list(map(lambda x: x / self.size(), self.graph_absolute()))
     
     def size(self):
         return self._size
     
     def free(self):
-        return self._graph[-1]
+        return self.size() - self.usage_absolute()
     
     def usage_absolute(self):
-        return self.size() - self.free()
+        return self._graph[-1]
     
     def usage_relative(self):
         return self.usage_absolute() / self.size()
 
     def tick(self):
-        self._graph.append(self.free())
+        self._graph.append(self._graph[-1])
     
     def store(self, file_size):
         assert file_size <= self.free()
-        self._graph[-1] -= file_size
+        self._graph[-1] += file_size
--- main.py
+++ main.py
@@ -15,10 +15,23 @@ def simulate(drive_sizes, file_size, strategy):
     return drives
 
 if __name__ == "__main__":
-    # TODO proper interface with graphical output
+    import matplotlib.pyplot as plt
+    import matplotlib
+    matplotlib.use("tkagg")
+
+    # TODO runtime input, GUI
     param_drive_sizes = [ 12, 16, 12, 7 ]
     param_file_size = 4
-    param_strategy = strategies.strategy_first
-
+    param_strategy = strategies.strategy_high_water
+    
     drives = simulate(param_drive_sizes, param_file_size, param_strategy)
-    print("\n".join(map(Drive.__str__, drives)))
+
+    fig, ax = plt.subplots()
+    for i, drive in enumerate(drives):
+        print(drive)
+        ax.plot(drive.graph_relative(), label = f'Drive {i} ({drive.size()})')
+    ax.set_title(param_strategy.__name__) 
+    ax.set_xlabel("No. files stored")
+    ax.set_ylabel("Usage (relative)")
+    ax.legend()
+    plt.show()
